797eebd04c0d342569ed7a8328e9ccd8ae4505e4
StheloNgcoborh01 <yebonganeza@gmail.com>
2026-07-04 20:52:27 +0200

    Remove MUI Button import from Navbar

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom';
 import '../styles/Navbar.css';
-import { Button } from '@mui/material';
 import Panel from '../context/Panel.jsx';
 import { useState, useEffect } from 'react';
 import { isLoggedIn, logout } from '../services/AuthServices';
